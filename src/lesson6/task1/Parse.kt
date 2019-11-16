80c817940e4136bfd2e4d01a64b03f0e6540b280
denis-shvetcov <deniska28082001@gmail.com>
2019-11-17 02:16:49 +0700

    3 lesson
    Signed-off-by: denis-shvetcov <deniska28082001@gmail.com>

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -90,8 +90,10 @@ fun dateStrToDigit(str: String): String {
     )
     if (Regex("""(\d+\s.+\s\d+)""").find(str) == null) return ""
     val date = str.split(" ")
-    if (date[0].toInt() > 31 || (date[1] == months[2] && date[0].toInt() > 29)
-        || (date[0].toInt() == 31 && date[0].toInt() != daysInMonth(months.indexOf(date[1]), date[2].toInt()))
+    if (Regex("""[\d]""").replace(date[0], "") != "" ||
+        Regex("""[\d]""").replace(date[2], "") != "" ||
+        date[0].toInt() > 31 || (date[1] == months[2] && date[0].toInt() > 29) ||
+        (date[0].toInt() == 31 && date[0].toInt() != daysInMonth(months.indexOf(date[1]), date[2].toInt()))
     ) return ""
     var month = ""
     if (date[1] in months) {
@@ -226,7 +228,7 @@ fun bestHighJump(jumps: String): Int {
 fun plusMinus(expression: String): Int {
     val list = expression.split(" ")
     if (Regex("""[\d]""").replace(list[0], "") != "" || expression.isEmpty()
-        || list.contains("") || Regex("""[\d\s\+\-]""").replace(expression,"")!=""
+        || list.contains("") || Regex("""[\d\s\+\-]""").replace(expression, "") != ""
     ) throw IllegalArgumentException()
     var sum = list[0].toInt()
     for (i in 1 until list.size - 1 step 2) {
@@ -276,15 +278,18 @@ fun mostExpensive(description: String): String {
     var max = Double.NEGATIVE_INFINITY
     var namemax = ""
     for (i in list.indices) {
-        if (Regex("""(\s\d+)""").find(list[i])!!.value.toDouble() >= max || (Regex("""(\s\d+\.\d+)""").find(list[i])!!.value.toDouble() >= max)) {
-            if (Regex("""(\s\d+\.\d+)""").find(list[i]) != null) max =
-                Regex("""(\s\d+\.\d+)""").find(list[i])!!.value.toDouble() else max =
-                Regex("""(\s\d+)""").find(list[i])!!.value.toDouble()
+        if (Regex("""(\s\d+\.\d+)""").find(list[i]) != null && Regex("""(\s\d+\.\d+)""").find(list[i])!!.value.toDouble() >= max) {
+            max = Regex("""(\s\d+\.\d+)""").find(list[i])!!.value.toDouble()
+            namemax = Regex("""(\s.*)""").replace(list[i], "")
+        }
+        if (Regex("""(\s\d+)""").find(list[i]) != null && Regex("""(\s\d+)""").find(list[i])!!.value.toDouble() >= max) {
+            max = Regex("""(\s\d+)""").find(list[i])!!.value.toDouble()
             namemax = Regex("""(\s.*)""").replace(list[i], "")
         }
     }
     return namemax
 }
+
 
 /**
  * Сложная
